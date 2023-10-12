ffc93cd3a44e3f22ad0bcab88322f519e13b6fea
Rible6 <rysio6@gmail.com>
2023-10-12 21:22:56 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<iostream>
+using namespace std;
+
+struct fibo
+{
+    int elo;
+    int bo;
+};
+int main()
+{
+    fibo d;
+    d.elo=5;
+    d.bo=2;
+    cout<<d.elo<<endl<<d.bo;
+}
